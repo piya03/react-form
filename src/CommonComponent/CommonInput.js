43d7fbd38ac09a6e2fb428dd722341a7433685cf
piya03 <piyachaudhary50@gmail.com>
2020-08-16 23:36:49 +0530

    create folder

---
+++ src/CommonComponent/CommonInput.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function CommonInput() {
+  return (
+    <div>
+      <div>
+        <label htmlFor="">Name</label>
+        <input type="text" />
+      </div>
+    </div>
+  );
+}
+
+export default CommonInput;
---
+++ src/Form.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+import CommonInput from "./CommonComponent/CommonInput";
+function Form() {
+  return (
+    <div className="App">
+      <CommonInput />
+    </div>
+  );
+}
+
+export default Form;
