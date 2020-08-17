40000976582ed793f501cd389489fde3e17aa205
piya03 <piyachaudhary50@gmail.com>
2020-08-17 23:53:49 +0530

    common input

--- src/CommonComponent/CommonInput.js
+++ src/CommonComponent/CommonInput.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
+import "./style.css";
 
-function CommonInput() {
+function CommonInput({ value, onChange, label }) {
+  const lableRef = useRef(null);
+  const inputRef = useRef(null);
+  useEffect(() => {
+    //inputRef.current.style.backgroundColor = "red";
+    console.log("CommonInput -> inputRef", inputRef);
+  });
+  //inputRef.current.backgroundColor = "red";
   return (
     <div>
-      <div>
-        <label htmlFor="">Name</label>
-        <input type="text" />
+      <div className="inputBox">
+        <label htmlFor="" className="inputLable" ref={lableRef}>
+          {label}
+        </label>
+        <input
+          ref={inputRef}
+          className="input"
+          type="text"
+          value={value}
+          onChange={onChange}
+        />
       </div>
     </div>
   );
--- src/Form.js
+++ src/Form.js
@@ -4,6 +4,10 @@ import CommonInput from "./CommonComponent/CommonInput";
 function Form() {
   return (
     <div className="App">
+      <CommonInput label="First Name" />
+      <CommonInput label="Last Name" />
+      <CommonInput />
+      <CommonInput />
       <CommonInput />
     </div>
   );
